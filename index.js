1c3d63334c560e0668648b1f671147e711934d3f
AnibalDiaz <anibalmauriciod@gmail.com>
2020-12-11 15:21:10 -0600

    con heroku

--- index.js
+++ index.js
@@ -7,13 +7,16 @@ var usuariosRouter = require('./routers/usuarios-router');
 const passport = require('passport');
 var flash = require('connect-flash');
 var session = require('express-session');
+const http = require('http');
+const server = http.createServer(app);
 
 var app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.set('port', process.env.PORT || 3000);
 
-//app.use(express.static('public'));
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -27,7 +30,6 @@ app.use(passport.session());
 app.use(flash());
 
 //middleware
-//app.use('/usuarios', usuariosRouter);
 
 //routes
 require('./models/routes')(app, passport);
@@ -35,7 +37,11 @@ require('./models/routes')(app, passport);
 //static files
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+/*
 app.listen(8888, ()=>{
     console.log('Servidor del backend levantado en 8888');
+});
+*/
+server.listen(app.get('port'), () => {
+    console.log(`server on port ${app.get('port')}`);
 });
